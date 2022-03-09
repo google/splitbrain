f825a108bafbb0c5b310a49d65f3117278581104
Cameron Brown <cambr@google.com>
2022-03-09 16:59:52 +0000

    Fix whitespace issues

--- main.py
+++ main.py
@@ -31,16 +31,13 @@ def _load_graphdef_from_file(path: str) -> program_graph_pb2.GraphDef:
 
 def _make_graph_from_proto(graphdef: program_graph_pb2.GraphDef) -> nx.Graph:
   graph = nx.DiGraph()
-
   graph.add_nodes_from([symbol.name for symbol in graphdef.symbol])
 
   for symbol in graphdef.symbol:
     for edge in symbol.u_edge:
       graph.add_edge(symbol.name, edge)
 
-
   graph.remove_edges_from(nx.selfloop_edges(graph))
-
   return graph 
 
 
