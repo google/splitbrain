f4cd363e1a5b9d9cb3415a9c66faef74eea9b62b
GreenishBlue <cambr@google.com>
2022-03-09 17:03:30 +0000

    Adds launcher binary for running over arbitrary inputs (#3)
    This change adds a launcher binary to run over an arbitrary program GraphDef.
    The input can be sourced from Kythe bindings, a BUILD graph or a source file
    dependency graph.
    
    Future additions will add generators for those, as well as a statistics module for
    counting and evaluating the quality of the algorithms.
    
    Full list of changes in this patch:
    * Updates .gitignore to include protobuf compiler outputs
    * Adds package statement to program_graph.proto
    * Adds binary scaffolding using absl-py flags
    * Remove optional fields due to proto3 syntax
    * Completes the launcher by loading in GraphDefs
    * Update pydoc and formatting to reflect final CL state

--- main.py
+++ main.py
@@ -4,10 +4,57 @@ Reads in a program graph and generates output symbols. See splitbrain.py for
 the algorithm itself.
 
 Usage:
-  ./splitbrain.py --input_graph=example_graph.textproto --output_dir=out
-
-TODO(cambr): Implement.
+  ./splitbrain.py --input_graph=example_graph.textproto
 """
 
+import os
+import networkx as nx
+import program_graph_pb2
+import splitbrain
+
+from absl import app
+from absl import flags
+from google.protobuf import text_format
+
+FLAGS = flags.FLAGS
+
+flags.DEFINE_string('input_path', None, 'Path to input GraphDef.')
+flags.mark_flag_as_required('input_path')
+
+
+def _load_graphdef_from_file(path: str) -> program_graph_pb2.GraphDef:
+  graphdef = program_graph_pb2.GraphDef()
+  with open(path, 'r') as f:
+    text_format.Merge(f.read(), graphdef)
+  return graphdef
+
+
+def _make_graph_from_proto(graphdef: program_graph_pb2.GraphDef) -> nx.Graph:
+  graph = nx.DiGraph()
+  graph.add_nodes_from([symbol.name for symbol in graphdef.symbol])
+
+  for symbol in graphdef.symbol:
+    for edge in symbol.u_edge:
+      graph.add_edge(symbol.name, edge)
+
+  graph.remove_edges_from(nx.selfloop_edges(graph))
+  return graph 
+
+
+def main(argv):
+  del argv
+
+  if not os.path.exists(FLAGS.input_path):
+    raise flags.FlagError("input path of %s is invalid".format(FLAGS.input_path))
+  graphdef = _load_graphdef_from_file(FLAGS.input_path)
+
+  G = _make_graph_from_proto(graphdef)
+  print("Loaded graphdef into memory: %s".format(G))
+
+  CLs = splitbrain.compute_sub_cls(G)
+  for changelist in CLs:
+    print('Changelist: ' + str(changelist))
+
+
 if __name__ == '__main__':
-  raise NotImplementedError("Not yet implemented - see splitbrain.py.")
+  app.run(main)
