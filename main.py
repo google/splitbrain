5c341da53732d62bb8a2a181150ce316ee90acc3
Cameron Brown <hello@cameronbrown.co.uk>
2022-03-20 20:58:22 +0000

    Update main.py

--- main.py
+++ main.py
@@ -17,6 +17,12 @@ from absl import app
 from absl import flags
 from google.protobuf import text_format
 
+# Map of valid algorithmic names and associated classes.
+VALID_ALGORITHMS = {
+  'Control': splitbrain.NullAlgorithm,
+  'SplitbrainV2': splitbrain.SplitbrainV2,
+}
+
 FLAGS = flags.FLAGS
 
 flags.DEFINE_string('input_path', None, 'Path to input GraphDef.')
@@ -24,17 +30,11 @@ flags.mark_flag_as_required('input_path')
 flags.DEFINE_bool('enable_statistics', False,
                   'If true, capture statistics for this session.')
 flags.DEFINE_string(
-    'output_dir', None,
-    'Directory to write output artefacts (e.g. statistics pb) to.')
-
-
-VALID_ALGORITHMS = {
-  'Control': splitbrain.NullAlgorithm,
-  'SplitbrainV2': splitbrain.SplitbrainV2,
-}
+  'output_dir', None,
+  'Directory to write output artefacts (e.g. statistics pb) to.')
 flags.DEFINE_multi_enum(
-    'algorithms', ['SplitbrainV2'], VALID_ALGORITHMS.keys(),
-    'Multi string list of algorithms to run, e.g. SplitbrainV2.')
+  'algorithms', ['SplitbrainV2'], VALID_ALGORITHMS.keys(),
+  'Multi string list of algorithms to run, e.g. SplitbrainV2.')
 
 
 def _load_graphdef_from_file(path: str) -> program_graph_pb2.GraphDef:
@@ -45,44 +45,44 @@ def _load_graphdef_from_file(path: str) -> program_graph_pb2.GraphDef:
 
 
 def _make_graph_from_proto(graphdef: program_graph_pb2.GraphDef) -> nx.Graph:
-    graph = nx.DiGraph()
-    graph.add_nodes_from([symbol.name for symbol in graphdef.symbol])
+  graph = nx.DiGraph()
+  graph.add_nodes_from([symbol.name for symbol in graphdef.symbol])
 
-    for symbol in graphdef.symbol:
-      for edge in symbol.u_edge:
-        graph.add_edge(symbol.name, edge)
+  for symbol in graphdef.symbol:
+    for edge in symbol.u_edge:
+      graph.add_edge(symbol.name, edge)
 
-    graph.remove_edges_from(nx.selfloop_edges(graph))
-    return graph
+  graph.remove_edges_from(nx.selfloop_edges(graph))
+  return graph
 
 
 def main(argv):
-    del argv
+  del argv
 
-    if not os.path.exists(FLAGS.input_path):
-      raise Exception("input path of %s is invalid" % (FLAGS.input_path))
-    graphdef = _load_graphdef_from_file(FLAGS.input_path)
+  if not os.path.exists(FLAGS.input_path):
+    raise Exception("input path of %s is invalid" % (FLAGS.input_path))
+  graphdef = _load_graphdef_from_file(FLAGS.input_path)
 
-    G = _make_graph_from_proto(graphdef)
-    print("Loaded graphdef into memory: %s" % G)
+  G = _make_graph_from_proto(graphdef)
+  print("Loaded graphdef into memory: %s" % G)
 
-    for algorithm_name in FLAGS.algorithms:
-      print("\nRunning algorithm: %s" % algorithm_name)
-      algorithm = VALID_ALGORITHMS[algorithm_name]()
+  for algorithm_name in FLAGS.algorithms:
+    print("\nRunning algorithm: %s" % algorithm_name)
+    algorithm = VALID_ALGORITHMS[algorithm_name]()
 
-      CLs = algorithm.run(G)
-      for changelist in CLs:
-        print('Changelist: ' + str(changelist))
+    CLs = algorithm.run(G)
+    for changelist in CLs:
+      print('Changelist: ' + str(changelist))
 
-      if FLAGS.enable_statistics:
-        if FLAGS.output_dir is None:
-          raise Exception(
-            "output_dir cannot be empty if --enable_statistics.")
-        print('Writing statistics to disk.')
-        stats_pb = statistics.evaluate(G, CLs)
-        output_path = os.path.join(FLAGS.output_dir, 'statistics.proto')
-        with open(output_path, 'wb') as f:
-          f.write(stats_pb.SerializeToString())
+    if FLAGS.enable_statistics:
+      if FLAGS.output_dir is None:
+        raise Exception(
+          "output_dir cannot be empty if --enable_statistics.")
+      print('Writing statistics to disk.')
+      stats_pb = statistics.evaluate(G, CLs)
+      output_path = os.path.join(FLAGS.output_dir, 'statistics.proto')
+      with open(output_path, 'wb') as f:
+        f.write(stats_pb.SerializeToString())
 
 
 if __name__ == '__main__':
