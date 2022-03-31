225c163c53a734aa930a5c2b0da3aca327a34dd5
Cameron Brown <cambr@google.com>
2022-03-31 20:19:16 +0100

    Uses topological sort in SplitbrainV2 for computing max changelists (#16)
    This change starts using topological sort instead of exhaustively extracting
    leaf nodes, which is the existing approach. This includes a basic (greedy)
    clustering/partitioning algorithm applied based upon a specified cost function
    which is also WIP.

---
+++ graphdef_utils_test.py
@@ -0,0 +1,25 @@
+from absl.testing import absltest
+import graphdef_utils
+import networkx as nx
+
+
+class GraphDefUtilsTest(absltest.TestCase):
+
+  def test_make_graph_from_proto(self):
+    graphdef = graphdef_utils.load_graphdef_from_file(
+        "testdata/example_graph.textproto")
+
+    G1 = nx.DiGraph()
+    G1.add_nodes_from(["fib", "my_error", "do_fib"])
+    G1.add_edge("fib", "fib")  # self loops.
+    G1.add_edge("do_fib", "my_error")
+    G1.add_edge("do_fib", "fib")
+    G1.remove_edges_from(nx.selfloop_edges(G1))
+
+    G2 = graphdef_utils.make_graph_from_proto(graphdef)
+
+    self.assertTrue(nx.is_isomorphic(G1, G2))
+
+
+if __name__ == '__main__':
+  absltest.main()
--- splitbrain.py
+++ splitbrain.py
@@ -14,18 +14,12 @@ See main.py for running the algorithm against input data.
 """
 
 import networkx as nx
+from pyparsing import empty
 import program_graph_pb2
 
 
 class SplitbrainAlgorithm:
   """Base implementation of the SplitBrain algorithm."""
-
-  def _find_leaf_nodes(self, graph):
-    """Next, find leaf nodes of the program symbols.
-
-    Iteratively remove them from the graph until at the final state.
-    """
-    return [v for v, d in graph.out_degree() if d == 0]
 
   def is_valid(self, graphdef: program_graph_pb2.GraphDef) -> bool:
     """Returns if the algorithm can run over the given graphdef."""
@@ -52,6 +46,9 @@ class SplitbrainV1(SplitbrainAlgorithm):
   symbolic data is fed.
   """
 
+  def _find_leaf_nodes(self, graph):
+    return [v for v, d in graph.out_degree() if d == 0]
+
   def is_valid(self, graphdef: program_graph_pb2.GraphDef) -> bool:
     for symbol in graphdef.symbol:
       if symbol.kind != program_graph_pb2.NodeDef.Kind.BUILD_TARGET:
@@ -74,18 +71,38 @@ class SplitbrainV1(SplitbrainAlgorithm):
 
 class SplitbrainV2(SplitbrainAlgorithm):
   """Naive implementation of the generalised (V2) SplitBrain algorithm.
-  
-  TODO(cameron): Add clustering.
+
+  Throws:
+    NetworkXError or NetworkXUnfeasible if the graph is undirected or is not a
+    complete directed acyclic graph.
   """
 
+  COST_THRESHOLD = 2
+
+  def cost(self, CL_symbols: list) -> float:
+    """Cost function for evaluating a CL.
+    
+    Args:
+      CL_symbols: List of symbols within a CL.
+    Returns:
+      Cost value."""
+    # TODO(cameron): Add a better cost fn.
+    return len(CL_symbols)
+
   def run(self, G: nx.Graph) -> list:
-    G = G.copy()
+    assert nx.is_directed_acyclic_graph(G)
+
+    # TODO(cameron): Apply DP approach instead of greedy algorithm.
     CLs = []
-    while len(G) > 0:
-      symbols = []
-      leaves = self._find_leaf_nodes(G)
-      for leaf in leaves:
-        G.remove_node(leaf)
-        symbols.append(leaf)
-      CLs.append(symbols)
+    CL = []
+    for symbol in list(reversed(list(nx.topological_sort(G)))):
+      print(symbol)
+      CL.append(symbol)
+      if self.cost(CL) >= SplitbrainV2.COST_THRESHOLD:
+        CLs.append(CL)
+        CL = []
+    
+    if len(CL) > 0:
+      CLs.append(CL)
+
     return CLs
